7881301104cea701e4e6ee4b821ad54209cb673c
Zeeshan Syed <zsyed89@gmail.com>
2026-03-02 13:16:49 -0500

    Added withLogger()

--- api-layer-async-operations/src/api/api.ts
+++ api-layer-async-operations/src/api/api.ts
@@ -13,7 +13,27 @@ const axiosInstance = axios.create(axiosParams);
 export const didAbort = (error: any) => axios.isCancel(error) && { aborted: true };
 export const isApiError = (error: any) => axios.isAxiosError(error);
 
-const withAbort = (fn: any) => {
+const withLogger = async <T>(promise: Promise<T>): Promise<T> => {
+  return promise.catch((error) => {
+    // Optional: Only log if an environment variable is set
+    if (!process.env.REACT_APP_DEBUG_API) throw error;
+
+    if (error.response) {
+      console.log("Response Error Data:", error.response.data);
+      console.log("Status Code:", error.response.status);
+      console.log("Headers:", error.response.headers);
+    } else if (error.request) {
+      console.log("No Response Received (Request):", error.request);
+    } else {
+      console.log("Request Setup Error:", error.message);
+    }
+    console.log("Axios Config:", error.config);
+
+    throw error;
+  });
+};
+
+const withAbort = (fn: any): ((...args: any[]) => Promise<AxiosResponse<any>>) => {
   return async (...args: any[]) => {
     const originalConfig = args[args.length - 1] || {};
     const { abort, ...config } = originalConfig;
@@ -43,19 +63,19 @@ const withAbort = (fn: any) => {
 
 const api = (axios: AxiosInstance) => ({
   get<T>(url: string, config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return withAbort(axios.get)(url, config);
+    return withLogger(withAbort(axios.get)(url, config));
   },
   post<T>(url: string, data: any, config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return withAbort(axios.post)(url, data, config);
+    return withLogger(withAbort(axios.post)(url, data, config));
   },
   put<T>(url: string, data: any, config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return withAbort(axios.put)(url, data, config);
+    return withLogger(withAbort(axios.put)(url, data, config));
   },
   patch<T>(url: string, data: any, config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return withAbort(axios.patch)(url, data, config);
+    return withLogger(withAbort(axios.patch)(url, data, config));
   },
   delete<T>(url: string, config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return withAbort(axios.delete)(url, config)
+    return withLogger(withAbort(axios.delete)(url, config));
   },
 });
 
