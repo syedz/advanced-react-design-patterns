dcf4f301c48af3b22e8275885617576cae431f67
Zeeshan Syed <zsyed89@gmail.com>
2026-03-02 08:10:04 -0500

    Converted split-screen to TypeScript

--- split-screen/src/App.tsx
+++ split-screen/src/App.tsx
@@ -1,26 +1,26 @@
-import './App.css'
+import "./App.css";
+import React from "react";
+import SplitScreen from "./components/SplitScreen.tsx";
 
-import SplitScreen from './components/SplitScreen'
-
-const LeftSideComponent = ({ title }: { title: string }) => {
-  return <h2 style={{ backgroundColor: 'red' }}>{title}</h2>
+interface SideProps {
+  title: string;
 }
 
-const RightSideComponent = ({ title }: { title: string }) => {
-  return <h2 style={{ backgroundColor: 'blue' }}>{title}</h2>
-}
+const LeftSideComponent: React.FC<SideProps> = ({ title }) => {
+  return <h2 style={{ backgroundColor: "red" }}>{title}</h2>;
+};
 
-function App() {
+const RightSideComponent: React.FC<SideProps> = ({ title }) => {
+  return <h2 style={{ backgroundColor: "blue" }}>{title}</h2>;
+};
 
+const App: React.FC = () => {
   return (
-    <SplitScreen 
-      leftWidth={1}
-      rightWidth={3}
-    >
+    <SplitScreen leftWidth={1} rightWidth={3}>
       <LeftSideComponent title="Left" />
       <RightSideComponent title="Right" />
     </SplitScreen>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- split-screen/src/components/SplitScreen.tsx
+++ split-screen/src/components/SplitScreen.tsx
@@ -1,27 +1,32 @@
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
+  display: flex;
 `;
 
-const Panel = styled.div<{ flex: number }>`
-    flex: ${(p) => p.flex}; // Accessing the flex prop dynamically
+const Panel = styled.div<{ $flex: number }>`
+  flex: ${(p) => p.$flex};
 `;
 
-type SplitScreenProps = {
-    leftWidth?: number;
-    rightWidth?: number;
-    children: [React.ReactNode, React.ReactNode];
+interface SplitScreenProps {
+  leftWidth?: number;
+  rightWidth?: number;
+  children: [React.ReactNode, React.ReactNode];
 }
 
-const SplitScreen = ({ children, leftWidth = 1, rightWidth = 1 }: SplitScreenProps) => {
-    const [left, right] = children;
-    return (
-        <Container>
-            <Panel flex={leftWidth}>{left}</Panel>
-            <Panel flex={rightWidth}>{right}</Panel>
-        </Container>
-    )
-}
+const SplitScreen: React.FC<SplitScreenProps> = ({
+  children,
+  leftWidth = 1,
+  rightWidth = 1,
+}) => {
+  const [left, right] = children;
+  return (
+    <Container>
+      <Panel $flex={leftWidth}>{left}</Panel>
+      <Panel $flex={rightWidth}>{right}</Panel>
+    </Container>
+  );
+};
 
 export default SplitScreen;
